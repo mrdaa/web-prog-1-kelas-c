5097db928b5a0e09c69bcfbd369b4402830ceb41
unknown <d.aji.apriyadi@gmail.com>
2018-02-28 16:45:35 +0700

    nambah file array-multi

---
+++ praktek-2/array-multi.php
@@ -0,0 +1,19 @@
+<?php
+$data =array(
+    "web-prog" =>array(
+        "dwi aji" =>7,
+        "Dono" =>8,
+        "kasino" => 6.
+    ),
+    "kalkulus"=> array(
+        "indro" => 9,
+        "bagito" => 8,
+        "benyamin" =>5
+    )
+);
+
+echo "Nilai web-prog untuk Dono : "
+    .$data["web-prog"]["kasino"] . "<br>";
+echo "Nilai Kalkulus untuk Indro : "
+    .$data["kalkulus"]["indro"];
+?>
