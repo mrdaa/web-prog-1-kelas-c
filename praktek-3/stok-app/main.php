9b7fb1ac286c2c691de795382ae652c2e1358b11
unknown <d.aji.apriyadi@gmail.com>
2018-03-21 10:16:38 +0700

    commit update selese

--- praktek-3/stok-app/main.php
+++ praktek-3/stok-app/main.php
@@ -11,14 +11,25 @@
 include "koneksi.php";
 $koneksiObj = new Koneksi();
 $koneksi = $koneksiObj->getKoneksi();
-if($koneksi->connect_error) {
-    echo "<tr><td>";
-    echo "Gagal koneksi : " . $koneksi->connect_error;
-    echo "</td></tr>";
-} else {
-    echo "<tr><td>";
-    echo "Sambungan basis data berhasil";
-    echo "</td></tr>";
+if ( $koneksi->connect_error) {
+ die ( "Koneksi ke Basis Data gagal : " . 
+        $koneksi->connect_error);}
+    
+$query = "select * from stok_barang";
+$result = $koneksi->query ($query);
+if ($result->num_rows > 0) {
+while($data = $result->fetch_assoc()){
+echo "<tr>" ;
+echo "<td>" . $data [ "kode" ] . "</td>" ;
+echo "<td>" . $data [ "nama_barang" ] . "</td>" ;
+echo "<td>" . $data [ "stok" ] . "</td>" ;
+echo "</tr>" ;
 }
-    ?>
+}else{
+echo "<tr>" ;
+echo '<td colspan="3">Data NIHIL</td>' ;
+echo "</tr>" ;
+}
+$koneksi->close();
+?>
 </table>
